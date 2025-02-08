2410afc1b68d1ca614d9212c1635dfdf39bcb28c
krolya <1c.korolev@gmail.com>
2025-02-08 16:13:00 +0100

    15

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,22 +46,18 @@ def upload_to_airtable(data):
     api = Api(st.secrets.AIRTABLE_API_TOKEN)
     table = api.table(st.secrets.AIRTABLE_BASE_ID, st.secrets.AIRTABLE_TABLE_ID)
     
-    response=table.create({        
-            "Name": "Иван Петров",
-            "Gender": "Мужской"
-        })
     #records = [{"fields": person} for person in data["records"]]]
-    #records = json.loads(data)
+    records = json.loads(data)
     #st.write(records)
     #st.write(len(records))
-    #for person in records:
+    #for person in records["records"]:
     #    st.write(person)
     #    response = table.create(person)
     #    st.write(response.json())
 
-    #response = table.batch_create(records)
+    response = table.batch_create(records["records"])
     st.write(response)
-    return 1
+    return len(response.json()["records"])
 
 with st.form("persona_form"):
     num_people = st.number_input("Количество персон", min_value=1, max_value=20, value=1)
@@ -76,7 +72,13 @@ if submitted:
         "rules": {{
             "fields": {{
                 "Name": "случайное имя по полу",
-                "Gender": "мужской/женский"
+                "Gender": "мужской/женский",
+                "Marital status id": "1 случайный ID из списка ["recB5tgjv9b2o2aPs", "recCO6SsPJva6VXmU", "recmMt8VCjPpC9O4I", "recEetmWrUmBJEh2I", "recTqqBXrv4PfjNDW"]",
+                "Children": "число ≥0 с учетом возраста",
+                "Income": "15-1500 тыс.руб",
+                "Age": "3-99 с логикой",
+                "Interests": "2 уникальных ID из списка ["rec0ExsZkszeavFi9", "recWPIWQQuEfQUfhC", "rec8DycBdP3nfIYAG", "recEdaM1lQmsr3Au8", "rec7kDVeNmJSleNKD"]",
+                "Description": "1-2 предложения с характером и интересами"
             }},
             "logic_checks": [
                 "Возраст >= 18 если есть дети",
@@ -90,14 +92,32 @@ if submitted:
     Верни структурированный JSON в формате и только его без каких либо пояснений или чего-либо еще в ответе: 
     
     {{
-        "records": [
-            {{
-            "name": "Иван Петров",
-            "gender": "Мужской"
+        "records":[
+        {{
+            "Name": "Иван Петров",
+            "Gender": "Мужской",
+            "Marital status ID": [
+            "recB5tgjv9b2o2aPs"
+            ],
+            "Children": 0,
+            "Income": 120000,
+            "Age": 28,
+            "Interests": [
+            "rec0ExsZkszeavFi9"
+            ]
         }},
         {{
-            "name": "Анна Смирнова",
-            "gender": "Женский"
+            "Name": "Анна Смирнова",
+            "Gender": "Женский",
+            "Marital status ID": [
+            "recCO6SsPJva6VXmU"
+            ],
+            "Children": 2,
+            "Income": 80000,
+            "Age": 35,
+            "Interests": [
+            "rechEsuoL1qsAFAiP"
+            ]
         }}
     ]
     }}'
@@ -112,8 +132,7 @@ if submitted:
         st.write(generated_data)
     
     st.success("Персонажи успешно сгенерированы!")
-    #st.json(generated_data)
-
+    
     with st.spinner("Загрузка в Airtable..."):
         uploaded_count = upload_to_airtable(generated_data)
     
